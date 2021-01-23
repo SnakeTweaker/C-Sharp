d732406ff98ade5c8bc2869d42a3c3ff74a1ed47
SnakeTweaker <77250853+SnakeTweaker@users.noreply.github.com>
2021-01-23 01:23:24 -0700

    Add files via upload

---
+++ Overflow/Overflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //practicing format strings
+            Console.WriteLine("This is a list of min and max values for primitive data types in C#. The left most value is min, right is max.");
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+            //Console.WriteLine("{0} {1}", bool.MinValue, bool.MaxValue); No min, no max
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", decimal.MinValue, decimal.MaxValue);
+            Console.WriteLine("{0} {1}", long.MinValue, long.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("{0} {1}", short.MinValue, short.MaxValue);
+
+            const float Pi = 3.14f;
+            //Pi = 1; -- This cannot be modified because it is a constant
+
+        }
+    }
+}
