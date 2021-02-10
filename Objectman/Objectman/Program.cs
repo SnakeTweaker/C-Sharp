07259ee7e41e91cb497d6fe63621229d7e3bd8f3
SnakeTweaker <77250853+SnakeTweaker@users.noreply.github.com>
2021-02-10 11:34:16 -0700

    Add files via upload

---
+++ Objectman/Objectman/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectman
+{
+    class Program
+    {
+        public class Person
+        {
+            int number;
+            Person person = new Person();
+            person.Name = "CJ";
+        }
+    }
+}
